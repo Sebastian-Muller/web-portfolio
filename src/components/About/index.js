a51eaaaee938219ab542f1a4fcd01724e03aabf8
Sebastian-Muller <seba.muller13@gmail.com>
2023-04-20 19:35:29 -0300

    feat: Contact page

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,18 +1,19 @@
 import './index.scss'
 import AnimatedLetters from '../AnimatedLetters'
 import { useState } from 'react'
-import { useEffect } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCss3, faGitAlt, faHtml5, faJsSquare, faNodeJs, faPython, faReact } from '@fortawesome/free-brands-svg-icons'
+import { faCss3, faGitAlt, faHtml5, faJsSquare, faNodeJs, faReact } from '@fortawesome/free-brands-svg-icons'
+import Loader from 'react-loaders'
 
 
 const About = () => {
 
     const [letterClass, setLetterClass] = useState('text-animate')
 
-    setTimeout(() => {setLetterClass('text-animate-hover')}, 3000)
+    setTimeout(() => { setLetterClass('text-animate-hover') }, 3000)
 
     return (
+        <>
         <div className='contauner about-page'>
             <div className='text-zone'>
                 <h1>
@@ -26,30 +27,32 @@ const About = () => {
                 <p>segundo parrafo</p>
                 <p>tercer parrafo</p>
             </div>
-        <div className='stafe-cube-cont'>
-            <div className='cubespinner'>
-                <div className='face1'>
-                    <FontAwesomeIcon icon={faReact} color='#5ed4f4' />
-                </div>
-                <div className='face2'>
-                    <FontAwesomeIcon icon={faHtml5} color='#f06529' />
-                </div>
-                <div className='face3'>
-                    <FontAwesomeIcon icon={faCss3} color='#28a4d' />
-                </div>
-                <div className='face4'>
-                    <FontAwesomeIcon icon={faJsSquare} color='#efd81d' />
-                </div>
-                <div className='face5'>
-                    <FontAwesomeIcon icon={faNodeJs} color='#8cc84b' />
-                </div>
-                <div className='face6'>
-                    <FontAwesomeIcon icon={faGitAlt} color='#ec428' />
+            <div className="stage-cube-cont">
+                <div className="cubespinner">
+                    <div className="face1">
+                        <FontAwesomeIcon icon={faHtml5} color="#F06529" />
+                    </div>
+                    <div className="face2">
+                        <FontAwesomeIcon icon={faNodeJs} color="#8CC64D" />
+                    </div>
+                    <div className="face3">
+                        <FontAwesomeIcon icon={faCss3} color="#28A4D9" />
+                    </div>
+                    <div className="face4">
+                        <FontAwesomeIcon icon={faGitAlt} color="#EC4D28" />
+                    </div>
+                    <div className="face5">
+                        <FontAwesomeIcon icon={faJsSquare} color="#EFD81D" />
+                    </div>
+                    <div className="face6">
+                        <FontAwesomeIcon icon={faReact} color="#5ED4F4" />
+                    </div>
                 </div>
             </div>
-        </div>
 
         </div>
+        <Loader type="pacman" />
+        </>
     )
 }
 
