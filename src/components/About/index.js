05080f98339383351d9c3c423aaa8fc419a50ec6
Sebastian-Muller <seba.muller13@gmail.com>
2023-04-21 11:31:28 -0300

    fix: Contact page

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -14,44 +14,44 @@ const About = () => {
 
     return (
         <>
-        <div className='contauner about-page'>
-            <div className='text-zone'>
-                <h1>
-                    <AnimatedLetters
-                        letterClass={letterClass}
-                        strArray={['A', 'b', 'o', 'u', 't', ' ', 'm', 'e']}
-                        idx={15}
-                    />
-                </h1>
-                <p>primer parrafo</p>
-                <p>segundo parrafo</p>
-                <p>tercer parrafo</p>
-            </div>
-            <div className="stage-cube-cont">
-                <div className="cubespinner">
-                    <div className="face1">
-                        <FontAwesomeIcon icon={faHtml5} color="#F06529" />
-                    </div>
-                    <div className="face2">
-                        <FontAwesomeIcon icon={faNodeJs} color="#8CC64D" />
-                    </div>
-                    <div className="face3">
-                        <FontAwesomeIcon icon={faCss3} color="#28A4D9" />
-                    </div>
-                    <div className="face4">
-                        <FontAwesomeIcon icon={faGitAlt} color="#EC4D28" />
-                    </div>
-                    <div className="face5">
-                        <FontAwesomeIcon icon={faJsSquare} color="#EFD81D" />
-                    </div>
-                    <div className="face6">
-                        <FontAwesomeIcon icon={faReact} color="#5ED4F4" />
+            <div className="container about-page">
+                <div className="text-zone">
+                    <h1>
+                        <AnimatedLetters
+                            letterClass={letterClass}
+                            strArray={['A', 'b', 'o', 'u', 't', ' ', 'm', 'e']}
+                            idx={15}
+                        />
+                    </h1>
+                    <p>primer parrafo</p>
+                    <p>segundo parrafo</p>
+                    <p>tercer parrafo</p>
+                </div>
+                <div className="stage-cube-cont">
+                    <div className="cubespinner">
+                        <div className="face1">
+                            <FontAwesomeIcon icon={faHtml5} color="#F06529" />
+                        </div>
+                        <div className="face2">
+                            <FontAwesomeIcon icon={faNodeJs} color="#8CC64D" />
+                        </div>
+                        <div className="face3">
+                            <FontAwesomeIcon icon={faCss3} color="#28A4D9" />
+                        </div>
+                        <div className="face4">
+                            <FontAwesomeIcon icon={faGitAlt} color="#EC4D28" />
+                        </div>
+                        <div className="face5">
+                            <FontAwesomeIcon icon={faJsSquare} color="#EFD81D" />
+                        </div>
+                        <div className="face6">
+                            <FontAwesomeIcon icon={faReact} color="#5ED4F4" />
+                        </div>
                     </div>
                 </div>
-            </div>
 
-        </div>
-        <Loader type="pacman" />
+            </div>
+            <Loader type="pacman" />
         </>
     )
 }
--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,22 +1,28 @@
 import Loader from 'react-loaders'
 import './index.scss'
+import { MapContainer, TileLayer, Marker } from 'react-leaflet'
 import AnimatedLetters from '../AnimatedLetters'
-import { useRef, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import emailjs from '@emailjs/browser'
+
 
 const Contact = () => {
 
     const [letterClass, setLetterClass] = useState('text-animate')
+    const form = useRef()
 
-    const refForm = useRef
+    useEffect(() => {
+        setTimeout(() => {
+            setLetterClass('text-animate-hover')
+        }, 3000)
+    }, [])
 
-    setTimeout(() => { setLetterClass('text-animate-hover') }, 3000)
 
     const sendEmail = (e) => {
         e.preventDefault()
 
         emailjs
-        .sendForm('YOUR_SERVICE_ID', 'YOUR_TEMPLATE_ID', form.current, 'YOUR_PUBLIC_KEY')
+            .sendForm('YOUR_SERVICE_ID', 'YOUR_TEMPLATE_ID', form.current, 'YOUR_PUBLIC_KEY')
             .then(
                 () => {
                     alert('Message successfully sent!')
@@ -40,11 +46,12 @@ const Contact = () => {
                             strArray={['C', 'o', 'n', 't', 'a', 'c', 't', ' ', 'm', 'e']}
                             idx={15} />
                     </h1>
+
                     <p>
                         Ver parrafo!!!
                     </p>
                     <div className='contact-form'>
-                        <form ref={refForm} onSubmit={sendEmail}>
+                        <form ref={form} onSubmit={sendEmail}>
                             <ul>
                                 <li className='half'>
                                     <input type='text' name='name' placeholder='Name' required />
@@ -65,7 +72,22 @@ const Contact = () => {
                     </div>
 
                 </div>
-
+                <div className='info-map'>
+                    Sebastian Müller
+                    <br />
+                    Argentina
+                    <br />
+                    Ciudad de Santa Fe, Santa Fe
+                    <br />
+                    <span>seba.muller13@gmail.com</span>
+                </div>
+                <div className='map-wrap'>
+                    <MapContainer center={[-31.63487, -60.70659]} zoom={13}>
+                        <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+                        <Marker position={[-31.63487, -60.70659]}>
+                        </Marker>
+                    </MapContainer>
+                </div>
             </div>
             <Loader type="pacman" />
         </>
